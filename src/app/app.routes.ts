a42ae6f332c732f12ed6a19fc192936debdd155f
Ionela Daniela <ionela3123@gmail.com>
2025-12-09 04:41:40 +0100

    Organización final del repositorio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { HomePage } from './features/home/home-page/home-page';
-import { CheckInPage } from './features/check-in/check-in-page/check-in-page';
-import { ReadingsPage } from './features/readings/readings-page/readings-page';
-import { JournalPage } from './features/journal/journal-page/journal-page';
-import { AboutPage } from './features/about/about-page/about-page';
-import { NotFoundPage } from './features/not-found/not-found-page/not-found-page';
+import { HomePage } from './components/home/home-page/home-page';
+import { CheckInPage } from './components/check-in/check-in-page/check-in-page';
+import { ReadingsPage } from './components/readings/readings-page/readings-page';
+import { JournalPage } from './components/journal/journal-page/journal-page';
+import { AboutPage } from './components/about/about-page/about-page';
+import { NotFoundPage } from './components/not-found/not-found-page/not-found-page';
 
 export const routes: Routes = [
   { path: 'home', component: HomePage },
